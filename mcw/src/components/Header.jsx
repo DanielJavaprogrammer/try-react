36dc504e3803df4cf20840758ed7f221b1808525
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-11-08 20:34:32 -0300

    Projeto inciado
    Inicio em 08/11/2025

---
+++ mcw/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="flex bg-blue-100 justify-around items-center">
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link></Link>
+          <Link></Link>
+          <Link></Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
