035e5d8865bc38d1ea293f19cc7ab63bc6e5b30a
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-11-26 15:10:53 -0300

    Landing Page 1.0
    Landing page was finished.

--- mcw/src/components/ScrollRevealWrapper/ScrollRevealWrapper.jsx
+++ mcw/src/components/ScrollRevealWrapper/ScrollRevealWrapper.jsx
@@ -1,23 +1,14 @@
-// src/components/ScrollRevealWrapper.jsx
-import React from "react";
-import useScrollReveal from "../hooks/useScrollReveal";
-// Importa o objeto de estilos
+import useScrollReveal from "../../hooks/useScrollReveal";
 import styles from "./ScrollReveal.module.css";
 
 const ScrollRevealWrapper = ({ children, ...options }) => {
   const [ref, isVisible] = useScrollReveal(options);
-
-  // Aplica as classes usando o objeto 'styles'
-  // O CSS Module irá converter 'scrollRevealContainer' para um nome único (ex: 'ScrollReveal_scrollRevealContainer__a1b2c')
   const containerClass = `${styles.scrollRevealContainer} ${
     isVisible ? styles.isVisible : ""
   }`;
 
   return (
-    <div
-      ref={ref} // Atribui a referência ao elemento DOM
-      className={containerClass}
-    >
+    <div ref={ref} className={containerClass}>
       {children}
     </div>
   );
